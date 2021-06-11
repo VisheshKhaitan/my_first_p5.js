4c1c7924ee9c3a0e4b354c4f5d5445baba093dc2
VisheshKhaitan <78291797+VisheshKhaitan@users.noreply.github.com>
2021-06-11 16:02:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw()
+{
+    image(video, 0, 0, 640, 480);
+}
+
+function take_snapshot()
+{
+    save('INTRO_TO_p5_image.png');
+}
